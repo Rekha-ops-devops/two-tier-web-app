6fea0f799654376b9be415257a57b0404a80e19a
Rekha <Rekha@gmail.com>
2026-09-19 05:44:09 -0600

    commit

--- backends.tf
+++ backends.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "tf-remote-bucket123"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "remote-db"
     encrypt        = true
   }
 }
